501f7635d7865ac4e5af7bcd854bfdecff000790
huhninho <lukas.hoerhan@extern.dkbcodefactory.com>
2023-10-21 11:59:22 +0200

    refactor ApiGatewayResponse

--- src/main/kotlin/com/cocktailfellow/ApiGatewayResponse.kt
+++ src/main/kotlin/com/cocktailfellow/ApiGatewayResponse.kt
@@ -2,29 +2,29 @@ package com.cocktailfellow
 
 import com.cocktailfellow.common.JsonConfig
 import kotlinx.serialization.encodeToString
-import java.util.*
 
 class ApiGatewayResponse(
   val statusCode: Int = 200,
   var body: String? = null,
-  val headers: Map<String, String>? = Collections.emptyMap(),
-  val isBase64Encoded: Boolean = false
+  val headers: Map<String, String>? = mapOf(
+    "X-Powered-By" to "AWS Lambda & serverless",
+    "Content-Type" to "application/json"
+  )
 ) {
   companion object {
-    val defaultHeaders = mapOf("X-Powered-By" to "AWS Lambda & serverless", "Content-Type" to "application/json")
 
     inline fun <reified T> withBody(status: Int, bodyObject: T): ApiGatewayResponse {
       val body = JsonConfig.instance.encodeToString(bodyObject)
-      return ApiGatewayResponse(status, body, defaultHeaders)
+      return ApiGatewayResponse(statusCode = status, body = body)
     }
 
     fun withoutBody(status: Int): ApiGatewayResponse {
-      return ApiGatewayResponse(status, body = null, defaultHeaders)
+      return ApiGatewayResponse(statusCode = status)
     }
 
     fun error(status: Int, message: String): ApiGatewayResponse {
       val body = JsonConfig.instance.encodeToString(mapOf("error" to message))
-      return ApiGatewayResponse(status, body, defaultHeaders)
+      return ApiGatewayResponse(statusCode = status, body = body)
     }
   }
 }
